a64ca3589bd8a1ace3904e6160a42740d7ccd7b0
andre-gcosta <87837752+andre-gcosta@users.noreply.github.com>
2021-09-14 15:53:28 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {    
+                numero: '17890',
+                nome: 'Candidato 1',
+                partido: 'ASL',
+                fotos: [
+                    {url:'17890.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '45222',
+                nome: 'Candidato 2',
+                partido: 'ZXY',
+                fotos:[
+                    {url:'45222.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {    
+                numero: '13786',
+                nome: 'Candidato 3',
+                partido: 'ABT',
+                fotos: [
+                    {url:'13786.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '13345',
+                nome: 'Candidato 4',
+                partido: 'ABT',
+                fotos:[
+                    {url:'13345.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '17',
+                nome: 'Fulano da Silva',
+                partido: 'ASL',
+                vice: 'Joao de Oliveira',
+                fotos: [
+                    {url:'17.jpg', legenda: 'Prefeito'},
+                    {url:'45.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Beltrano Gomes',
+                partido: 'ABT',
+                vice: 'Mateus Pereira',
+                fotos: [
+                    {url:'13.jpg', legenda: 'Prefeito'},
+                    {url:'51.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
